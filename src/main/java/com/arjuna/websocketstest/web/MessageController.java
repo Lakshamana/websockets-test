e63666e79c7aa7eab16f8b8f64dd1ec8c2051086
Lakshamana <guitrompa1@gmail.com>
2019-03-11 13:51:31 -0300

    Change return type to list of WebsocketMessage

--- src/main/java/com/arjuna/websocketstest/web/MessageController.java
+++ src/main/java/com/arjuna/websocketstest/web/MessageController.java
@@ -1,8 +1,9 @@
 package com.arjuna.websocketstest.web;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.concurrent.atomic.AtomicInteger;
 import com.arjuna.websocketstest.model.WebsocketMessage;
 
 import org.slf4j.Logger;
@@ -10,53 +11,36 @@ import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class MessageController {
     
-    private List<String> list;
+    private List<WebsocketMessage> list;
+
+    private AtomicInteger counter;
 
     private final Logger log = LoggerFactory.getLogger(MessageController.class);
     public MessageController() {
-        list = Arrays.asList();
+        list = new ArrayList<>();
+        counter = new AtomicInteger(-1);
     } 
     
     @MessageMapping("/send/message")
     @SendTo("/chat")
-    public WebsocketMessage receiveMessage(@Payload WebsocketMessage webMessage) {
-        log.debug("Message received: {}", webMessage);
-        // System.out.println("Message received: " + webMessage);
-        // if(webMessage.getType().equals("SEND")){
-        //     webMessage.setId(list.size() + 1);
-        //     list.add(webMessage.getContent());
-        //     list.stream().forEach(log::debug);
-        // } else {
-            
-        // }
-        return webMessage;
-    }
-
-    // @PostMapping("/messages")
-    // public ResponseEntity<String> addMessage(@RequestParam String message) {
-    //     this.list.add(message);
-    //     return ResponseEntity.ok().build();
-    // }
-
-    @GetMapping("/messages")
-    public List<String> getAllMessages(String message) {
+    public List<WebsocketMessage> receiveMessage(@Payload WebsocketMessage webMessage) {
+        log.info("Message received: {}", webMessage);
+        if(webMessage.getType().equals("SEND")){
+            webMessage.setId(counter.incrementAndGet());
+            this.list.add(webMessage);
+            log.info("LIST AFTER ADD: {}", this.list);
+        } else {
+            this.list.removeIf(message -> message.getId() == webMessage.getId());
+            log.info("LIST AFTER REMOVES: {}", this.list);
+        }
         return this.list;
     }
-
-    // @DeleteMapping("/messages/{id}")
-    // public ResponseEntity<String> removeMessage(@PathVariable int id) {
-    //     try {
-    //         this.list.remove(id);
-    //     } catch (Exception e) {
-    //         return ResponseEntity.badRequest().build();
-    //     }
-    //     return ResponseEntity.ok().build();
-    // }
 }
